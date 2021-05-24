48bdf3f0df4c2c0a5c4f5b4ac44ce2f20c2e9b39
Gregory Bertrand <gregoryalexbertrand@gmail.com>
2021-05-24 17:01:44 -0500

    get all stories is working

--- controllers/stories_controller.py
+++ controllers/stories_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import models
 
 def create_story():
+    print(request.json)
     user = models.User.query.filter_by(id=request.headers['Authorization']).first()
     prompt = models.Prompt.query.filter_by(id=request.json['prompt_id']).first()
     story = models.Story(
@@ -13,3 +14,7 @@ def create_story():
     models.db.session.add(story)
     models.db.session.commit()
     return{ "story": story.to_json()}
+
+def stories_index():
+    stories = models.Story.query.all()
+    return { "stories": [s.to_json() for s in stories]}
--- routes/stories_routes.py
+++ routes/stories_routes.py
@@ -2,3 +2,4 @@ from controllers import stories_controller
 
 def apply_stories_routes(app):
     app.route('/stories', methods=["POST"])(stories_controller.create_story)
+    app.route('/allstories', methods=["Get"])(stories_controller.stories_index)
